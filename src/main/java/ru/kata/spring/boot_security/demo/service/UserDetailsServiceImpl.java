5ee80b48ba830d225172046708291e11c5d25e3c
Andrey <147320074+ilupat@users.noreply.github.com>
2023-12-24 19:15:18 +0300

    fixed

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserService userService;
